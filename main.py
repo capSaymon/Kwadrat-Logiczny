02533868054f2d11e1fa636934e75b8dbf8adcf7
capSaymon <szymonpietruszka@onet.pl>
2025-03-27 17:26:50 +0100

    Zrobienie podmiotu i predyktu

--- main.py
+++ main.py
@@ -1,11 +1,46 @@
 from ollama import chat
 from ollama import ChatResponse
 
+sentence: str = 'Samolot stoi na płycie lotniska'
+
+prompt: str = f"""
+Dane wejściowe:  
+Zdanie: "{sentence}"  
+Zadanie: Podziel zdanie na podmiot i predykt. 
+Wymagania:
+- zawsze w 2 linijkach rozpisz odpowiedź
+- napisz w odpowiedzi podmiot: oraz predykt: 
+- nic nie zmieniaj
+- odpowiedzi to słowa nie zmienione z podanego zdania
+"""
+
 response: ChatResponse = chat(model='llama3.2:1b', messages=[
   {
     'role': 'user',
-    'content': input('User: '),
+    'content': prompt,
   },
 ])
 
-print(response.message.content)
+subject = ''
+prediction = ''
+
+save: bool = False
+
+for x in response.message.content.split():
+    if x.lower() == 'podmiot:' and not subject:
+        subject += ' '
+        continue
+    elif x.lower() == 'predykt:' or x.lower() == 'predyk:':
+        prediction += ' '
+        continue
+        
+    if subject and not prediction:
+        subject += x + ' '
+    elif prediction:
+        prediction += x + ' '
+
+subject = subject[1:]
+prediction = prediction[1:]
+
+print(subject)
+print(prediction)

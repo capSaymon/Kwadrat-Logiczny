5b0fbe69022d4f4d5047548fb366d7e8fe2ab453
capSaymon <szymonpietruszka@onet.pl>
2025-04-07 18:37:33 +0200

    male poprawki

--- backend/sentences.py
+++ backend/sentences.py
@@ -5,6 +5,37 @@ class SentencesPrompt():
         self.file_name = file_name
 
     def send(self):
+        #zero-shot
+        prompt: str = f"""
+        Kwadrat logiczny ma cztery wierzchołki oznaczone A, E, I oraz O
+
+        - Wierzchołek A to zdanie ogólno-twierdzące (np. „Wszystkie X są Y”)
+        - Wierzchołek E to zdanie ogólno-przeczące (np. „Żadne X nie jest Y”)
+        - Wierzchołek I to zdanie szczegółowo-twierdzące (np. „Niektóre X są Y”)
+        - Wierzchołek O to zdanie szczegółowo-przeczące (np. „Niektóre X nie są Y”)
+
+        Między tymi wierzchołkami zachodzą relacje logiczne:
+        
+        Implikacja:  
+        - A implikuje I jeśli A jest prawdziwe, to I jest prawdziwe
+        - E implikuje O jeśli E jest prawdziwe, to O jest prawdziwe
+
+        Przeciwieństwo:
+        - A i E nie mogą być jednocześnie prawdziwe, ale mogą być jednocześnie fałszywe
+
+        Sprzeczność: 
+        - A i O nie mogą być jednocześnie prawdziwe ani jednocześnie fałszywe  
+        - E i I nie mogą być jednocześnie prawdziwe ani jednocześnie fałszywe
+
+        Podprzeciwieństwo: 
+        - I i O nie mogą być jednocześnie fałszywe, ale mogą być jednocześnie prawdziwe     
+
+        Teraz, rozwiąż ten kwadrat logiczny. Podaj tylko reszte zdań i nie podawaj nic więcej. Bierz pod uwagę tylko te zasady:
+        
+        """
+
+
+        #few-shot
         prompt: str = f"""
         Kwadrat logiczny ma cztery wierzchołki oznaczone A, E, I oraz O
 
@@ -45,36 +76,8 @@ class SentencesPrompt():
         Zdanie O: Niektóre komputery nie są podłączone do Internetu.
 
         #Zadanie
-        Dokończ reszte zdań przykładu 3 tylko. Nie tłumacz relacji.
-        Przykład 3:
-        
-        """
-
-        prompt: str = f"""
-        Kwadrat logiczny ma cztery wierzchołki oznaczone A, E, I oraz O
-
-        - Wierzchołek A to zdanie ogólno-twierdzące (np. „Wszystkie X są Y”)
-        - Wierzchołek E to zdanie ogólno-przeczące (np. „Żadne X nie jest Y”)
-        - Wierzchołek I to zdanie szczegółowo-twierdzące (np. „Niektóre X są Y”)
-        - Wierzchołek O to zdanie szczegółowo-przeczące (np. „Niektóre X nie są Y”)
-
-        Między tymi wierzchołkami zachodzą relacje logiczne:
-        
-        Implikacja:  
-        - A implikuje I jeśli A jest prawdziwe, to I jest prawdziwe
-        - E implikuje O jeśli E jest prawdziwe, to O jest prawdziwe
-
-        Przeciwieństwo:
-        - A i E nie mogą być jednocześnie prawdziwe, ale mogą być jednocześnie fałszywe
-
-        Sprzeczność: 
-        - A i O nie mogą być jednocześnie prawdziwe ani jednocześnie fałszywe  
-        - E i I nie mogą być jednocześnie prawdziwe ani jednocześnie fałszywe
-
-        Podprzeciwieństwo: 
-        - I i O nie mogą być jednocześnie fałszywe, ale mogą być jednocześnie prawdziwe     
-
         Teraz, rozwiąż ten kwadrat logiczny. Podaj tylko reszte zdań i nie podawaj nic więcej. Bierz pod uwagę tylko te zasady:
+        Przykład 3:
         
         """
 
@@ -83,3 +86,7 @@ class SentencesPrompt():
 
         result = Prompt(f'{prompt} \n\n {task}').send()
         return task, result
+    
+    def save(self, answear):
+        with open(f'G:\Kwadrat Logiczny\Kwadrat-Logiczny\questions\{self.file_name}.txt', 'a', encoding='utf-8') as file:
+            file.write('\nAnswear:\n'+answear)
--- main.py
+++ main.py
@@ -1,9 +1,38 @@
 from backend.sentences import SentencesPrompt
+import os
+
+QUESTIONS_PATH = r'G:\Kwadrat Logiczny\Kwadrat-Logiczny\questions'
 
 def main ():
-    task = SentencesPrompt('question_1')
-    question, outcome = task.send()
-    print(question, '\n-----------------------------------------------------------\n', outcome)    
+    for file_name in os.listdir(QUESTIONS_PATH):
+        if not file_name.endswith('.txt'):
+            continue
+        base_name = os.path.splitext(file_name)[0]
+        print(f'\nFile: {base_name}')
+
+        run = True
+        while run:
+            task = SentencesPrompt(base_name)
+            question, outcome = task.send()
+            if 'Answear:' in question:
+                print(f'File {base_name} allready have answear')
+                break
+            print(question,'\n\n', outcome)
+
+            while True:
+                action = input('Reject or Accept (r/a): ')
+                if action == 'a':
+                    print('Accept and save answear \n')
+                    task.save(outcome)
+                    run = False
+                    break
+                elif action == 'r':
+                    print('Reject and not save answear\n')
+                    break
+                else:
+                    print('Error. Try again')
+        print('\n','-'*50,'\n')
+
 
 if __name__ == '__main__':
     main()
